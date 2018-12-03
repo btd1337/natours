b1ee6d89ae89b97749d1f8ec509a75af1fda13a6
btd1337 <helder.bertoldo@ice.ufjf.br>
2018-12-03 01:00:52 -0200

    Create front card

--- gulpfile.js
+++ gulpfile.js
@@ -1,164 +1,215 @@
-const autoprefixer = require('gulp-autoprefixer');
-const browserSync = require('browser-sync');
+const autoprefixer = require('autoprefixer');
+const browserSync = require('browser-sync').create();
 const cache = require('gulp-cache');
 const concat = require('gulp-concat');
+const cssnano = require('cssnano');
 const del = require('del'); // rm -rf
-const ghPages = require('@justeat/gulp-gh-pages');
+const ghPages = require('gulp-gh-pages');
 const gulp = require('gulp');
 const imagemin = require('gulp-imagemin');
 const jsmin = require('gulp-jsmin');
 const minifycss = require('gulp-minify-css');
 const plumber = require('gulp-plumber');
+const postcss = require('gulp-postcss');
+const reload = browserSync.reload;
 const rename = require('gulp-rename');
 const sass = require('gulp-sass');
-const sequence = require('gulp-sequence');
+const sourcemaps = require('gulp-sourcemaps');
 
-var DEST = './src';
+var DEST = 'src/';
 
-gulp.task('browser-sync', function () {
-	browserSync({
-		server: {
-			baseDir: './src'
-		}
-	});
+var paths = {
+  project: {
+    src: 'src/',
+    dest_dir: 'dist/',
+    dest_files: 'dist/**/*',
+  },
+  html: {
+    src: 'src/**/*.html',
+    dest: DEST,
+  },
+  images: {
+    src: 'src/assets/img/**/*',
+    dest: DEST.concat('assets/img/'),
+  },
+  scripts: {
+    src: 'src/assets/js/**/*.js',
+    dest: DEST.concat('assets/js/3rd'),
+  },
+  scripts_3rd: {
+    src: ['./node_modules/vue/dist/vue.min.js'],
+    dest: DEST.concat('assets/js'),
+  },
+  styles_css: {
+    src: 'src/assets/css/*.css',
+    dest: DEST.concat('assets/css/'),
+  },
+  styles_sass: {
+    src: 'src/assets/scss/**/*.scss',
+    dest: DEST.concat('assets/css/'),
+  },
+  styles_3rd: {
+    src: 'src/assets/css/3rd/**/*',
+    dest: DEST.concat('assets/css/3rd/'),
+  },
+  styles_bootstrap: {
+    src: './node_modules/bootstrap/scss/bootstrap.scss',
+    dest: DEST.concat('assets/css/3rd/'),
+  },
+};
+
+function updatePaths() {
+  paths.html.dest = DEST;
+  paths.images.dest = DEST.concat('assets/img/');
+  paths.scripts.dest = DEST.concat('assets/js/3rd');
+  paths.scripts_3rd.dest = DEST.concat('assets/js');
+  paths.styles_3rd.dest = DEST.concat('assets/css/3rd/');
+  paths.styles_bootstrap.dest = DEST.concat('assets/css/3rd/');
+  paths.styles_css.dest = DEST.concat('assets/css/');
+  paths.styles_sass.dest = DEST.concat('assets/css/');
+}
+
+gulp.task('images', (done) => {
+  gulp
+    .src(paths.images.src)
+    .pipe(
+      cache(
+        imagemin({
+          optimizationLevel: 3,
+          progressive: true,
+          interlaced: true,
+        }),
+      ),
+    )
+    .pipe(gulp.dest(paths.images.dest));
+  done();
 });
 
-gulp.task('bs-reload', function () {
-	browserSync.reload();
+gulp.task('sass', (done) => {
+  gulp
+    .src([paths.styles_sass.src])
+    .pipe(sourcemaps.init())
+    .pipe(sass())
+    .on('error', sass.logError)
+    .pipe(postcss([autoprefixer(), cssnano()]))
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest(paths.styles_sass.dest));
+  done();
 });
 
-gulp.task('images', function () {
-	gulp
-		.src('src/assets/img/**/*')
-		.pipe(
-			cache(
-				imagemin({
-					optimizationLevel: 3,
-					progressive: true,
-					interlaced: true
-				})
-			)
-		)
-		.pipe(gulp.dest(DEST + '/assets/img/'));
-});
-
-gulp.task('sass', function () {
-	gulp
-		.src(['./src/assets/scss/**/*.scss'])
-		.pipe(
-			plumber({
-				errorHandler: function (error) {
-					console.log(error.message);
-					this.emit('end');
-				}
-			})
-		)
-		.pipe(sass())
-		.pipe(autoprefixer('last 2 versions'))
-		.pipe(gulp.dest(DEST + '/assets/css/'))
-		.pipe(
-			rename({
-				suffix: '.min'
-			})
-		)
-		.pipe(minifycss())
-		.pipe(gulp.dest(DEST + '/assets/css/'))
-		.pipe(
-			browserSync.reload({
-				stream: true
-			})
-		);
-});
-
-gulp.task('third-party-css', function () {
-	gulp
-		.src('src/assets/css/third-party/**/*')
-		.pipe(gulp.dest(DEST + '/assets/css/third-party/'));
+gulp.task('third-party-css', (done) => {
+  gulp.src(paths.styles_3rd.src).pipe(gulp.dest(paths.styles_3rd.dest));
+  done();
 });
 
 // Compile Bootstrap SASS files into CSS FILES
-gulp.task('bootstrap', () => {
-	gulp
-		.src(['./node_modules/bootstrap/scss/bootstrap.scss'])
-		.pipe(
-			plumber({
-				errorHandler: function (error) {
-					console.log(error.message);
-					this.emit('end');
-				}
-			})
-		)
-		.pipe(sass())
-		.pipe(autoprefixer('last 2 versions'))
-		.pipe(gulp.dest(DEST + '/assets/css/'))
-		.pipe(
-			rename({
-				suffix: '.min'
-			})
-		)
-		.pipe(minifycss())
-		.pipe(gulp.dest(DEST + '/assets/css/'))
-		.pipe(
-			browserSync.reload({
-				stream: true
-			})
-		);
+gulp.task('bootstrap', (done) => {
+  gulp
+    .src([paths.styles_bootstrap.src])
+    .pipe(
+      plumber({
+        errorHandler: function(error) {
+          console.log(error.message);
+          this.emit('end');
+        },
+      }),
+    )
+    .pipe(sass())
+    .pipe(gulp.dest(paths.styles_bootstrap.dest))
+    .pipe(
+      rename({
+        suffix: '.min',
+      }),
+    )
+    .pipe(minifycss())
+    .pipe(gulp.dest(paths.styles_bootstrap.dest))
+    .pipe(
+      browserSync.reload({
+        stream: true,
+      }),
+    );
+  done();
 });
 
 // Compile External JS files into one file
-gulp.task('lib', () => {
-	gulp
-		.src([
-			'./node_modules/jquery/dist/jquery.min.js',
-			'./node_modules/vue/dist/vue.min.js'
-		])
-		.pipe(concat('lib.js'))
-		.pipe(jsmin())
-		.pipe(rename({
-			suffix: '.min'
-		}))
-		.pipe(gulp.dest(DEST + '/assets/js/'));
+gulp.task('lib', (done) => {
+  gulp
+    .src(paths.scripts_3rd.src)
+    .pipe(concat('lib.js'))
+    .pipe(jsmin())
+    .pipe(
+      rename({
+        suffix: '.min',
+      }),
+    )
+    .pipe(gulp.dest(paths.scripts_3rd.dest));
+  done();
 });
 
 // Compile JS files into one file
-gulp.task('js', () => {
-	gulp
-		.src('./src/assets/js/**/*.js')
-		.pipe(concat('main.js'))
-		.pipe(jsmin())
-		.pipe(rename({
-			suffix: '.min'
-		}))
-		.pipe(gulp.dest(DEST + '/assets/js/'));
+gulp.task('js', (done) => {
+  gulp
+    .src(paths.scripts.src)
+    .pipe(concat('main.js'))
+    .pipe(jsmin())
+    .pipe(
+      rename({
+        suffix: '.min',
+      }),
+    )
+    .pipe(gulp.dest(paths.scripts.dest));
+  done();
 });
 
-gulp.task('html', () => {
-	gulp.src('src/**/*.html').pipe(gulp.dest(DEST));
+gulp.task('html', (done) => {
+  gulp.src(paths.html.src).pipe(gulp.dest(paths.html.dest));
+  done();
+});
+
+gulp.task('serve', () => {
+  browserSync.init({
+    server: {
+      baseDir: './src',
+    },
+  });
+  gulp.watch(paths.html.src).on('change', reload);
+  gulp.watch(paths.styles_sass.src).on('change', gulp.series('sass', reload));
 });
 
 // Clean dist folder before rebuild
-gulp.task('clean', function () {
-	return del(['./dist']);
+gulp.task('clean', (done) => {
+  del([paths.project.dest_dir]);
+  done();
 });
 
-gulp.task('default', ['develop', 'sass', 'browser-sync'], function () {
-	gulp.watch('src/assets/scss/**/*.scss', ['sass', 'bs-reload']);
-	gulp.watch('src/assets/js/**/*.js', ['js']);
-	gulp.watch('src/*.html', ['bs-reload']);
+gulp.task('develop', (done) => {
+  DEST = paths.project.src;
+  updatePaths();
+  done();
 });
 
-
-gulp.task('build', sequence('clean', ['production', 'sass', 'third-party-css', 'js', 'images', 'html']));
-
-gulp.task('develop', () => {
-	DEST = './src';
+gulp.task('production', (done) => {
+  DEST = paths.project.dest_dir;
+  updatePaths();
+  done();
 });
 
-gulp.task('production', () => {
-	DEST = './dist';
+gulp.task('deploy', function() {
+  return gulp.src(paths.project.dest_files).pipe(ghPages());
 });
 
-gulp.task('deploy', function () {
-	return gulp.src('./dist/**/*')
-		.pipe(ghPages());
-});
+gulp.task(
+  'default',
+  gulp.series('develop', 'sass', 'serve', () => {
+    gulp.watch(paths.scripts.src, ['js']);
+  }),
+);
+
+gulp.task(
+  'build',
+  gulp.series(
+    'clean',
+    'production',
+    gulp.parallel('sass', 'third-party-css', 'js', 'images', 'html'),
+  ),
+);
